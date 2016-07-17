f0466b8fc500147c18c96a977cae632ebfa02da2
n3tsky <n3tsky@protonmail.com>
2016-07-17 10:50:01 +0200

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+echo "[!] Install pip and cheat"
+sudo apt-get install -y python-pip
+pip install --upgrade pip
+sudo pip install cheat
+
+echo "[!] Set parameters"
+echo -e "\nEDITOR=vim" >> ~/.bashrc
+echo -e "\nCHEATCOLORS=true" >> ~/.bashrc
+if [ ! -d "~/.cheat" ]; then
+  mkdir "~/.cheat"
+fi
+
+echo "[!] Copy cheatsheets"
